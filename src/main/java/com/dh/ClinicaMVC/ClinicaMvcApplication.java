63fe588d9eaf3d80792866d9bb3fffc86cfaba0e
MP Data - Trading <mpiccato@me.com>
2025-11-20 06:43:27 -0300

    Repositorio creado

---
+++ src/main/java/com/dh/ClinicaMVC/ClinicaMvcApplication.java
@@ -0,0 +1,13 @@
+package com.dh.ClinicaMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaMvcApplication.class, args);
+	}
+
+}
