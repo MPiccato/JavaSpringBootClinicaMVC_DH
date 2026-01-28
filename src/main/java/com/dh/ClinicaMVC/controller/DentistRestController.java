501f90f3a4a9f434880a0d1d1e3866bacc191fec
MP Data - Trading <mpiccato@me.com>
2026-01-28 10:50:54 -0300

    CRUD de ApiRestFul funcionando

--- src/main/java/com/dh/ClinicaMVC/controller/DentistRestController.java
+++ src/main/java/com/dh/ClinicaMVC/controller/DentistRestController.java
@@ -4,8 +4,10 @@ import com.dh.ClinicaMVC.model.Dentist;
 import com.dh.ClinicaMVC.service.DentistService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/odontologos")
+@RequestMapping("/odontologo")
 public class DentistRestController {
 
     private DentistService dentistService;
@@ -18,6 +20,30 @@ public class DentistRestController {
     public Dentist getDentistById(@PathVariable Integer id){
         return dentistService.findByIdDentist(id);
 
+    }
+
+    //Guardar un nuevo dentista en la base de datos
+    @PostMapping("/guardar")
+    public Dentist guardarDentista(@RequestBody Dentist dentist) {
+        return dentistService.saveDentist(dentist);
+    }
+
+    //Modificar un dentista existente
+    @PutMapping("/actualizar")
+    public void actualizarDentista(@RequestBody Dentist dentist) {
+        dentistService.updateDentist(dentist);
+    }
+
+    //Borrar un dentista por su ID
+    @DeleteMapping("/borrar/{id}")
+    public void borrarDentista(@PathVariable Integer id) {
+        dentistService.deleteDentist(id);
+    }
+
+    //Listar todos los dentistas
+    @GetMapping("/listar")
+    public List<Dentist> listarDentistas() {
+        return dentistService.findAllDentist();
     }
 
 
